e6b17a1eb730116d551cbe24bc879abce249b6da
cealfaro <cealfaro@gmail.com>
2025-03-27 10:17:29 -0300

    compilacion de la app

--- BoletaHonorario/src/controlador/Main.java
+++ BoletaHonorario/src/controlador/Main.java
@@ -5,16 +5,10 @@ import vista.VistaInicio;
 public class Main {
     
     public static void main(String[] args) {
-        
-        
+
         VistaInicio prin = new VistaInicio();
         prin.setVisible(true);
         prin.setLocationRelativeTo(null);
         
-        
-        
-        
-        
     }
-    
 }
